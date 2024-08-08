8d465619ca2f34ccd08405c4f9cee448f17e3af2
Caballero-Sacro-I <direccionsistemas@cemdi.net.co>
2024-08-08 10:27:13 -0500

    Se modificaron los origenes

--- public/login.js
+++ public/login.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', async (event) =>
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    const response = await fetch('http://localhost:3000/api/login', {
+    const response = await fetch('https://cemditools-backend-cold-lake-6201.fly.dev/api/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
