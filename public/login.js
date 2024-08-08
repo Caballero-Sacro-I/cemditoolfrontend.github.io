f171a04d037bad11269e943c784e00b106c5e4f9
Caballero-Sacro-I <direccionsistemas@cemdi.net.co>
2024-08-08 09:20:57 -0500

    Se agrego el archivo index

---
+++ public/login.js
@@ -0,0 +1,23 @@
+document.getElementById('loginForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('https://cemditools-backend-cold-lake-6201.fly.dev/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    });
+
+    const data = await response.json();
+
+    if (response.status === 200) {
+        window.location.href = 'report.html'; // Redirige a la página inicial si el login es exitoso
+    } else {
+        alert(data.message); // Muestra una alerta si las credenciales son incorrectas
+        document.getElementById('message').innerText = data.message;
+    }
+});
---
+++ public/script.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM completamente cargado y parseado.");
+
+    // URL de embed de Power BI
+    const embedUrl = "https://app.powerbi.com/view?r=eyJrIjoiNDY4N2VhNjktZTYxOS00Njc1LTkzNjUtMjg3N2VkY2FmZGIzIiwidCI6IjUzOWIzYWNkLTE1MDQtNGUzNC1iODVjLTAyNGFlMTc4OTQxZCIsImMiOjR9";
+
+    const models = window['powerbi-client'].models;
+    const config = {
+        type: 'report',
+        tokenType: models.TokenType.Embed,
+        accessToken: "539b3acd-1504-4e34-b85c-024ae178941d", // El token generado
+        embedUrl: embedUrl,
+        id: "9d8ba5cf-b67c-44f0-9f39-4ba871ff3ca3", // El ID del informe
+        permissions: models.Permissions.All,
+        settings: {
+            filterPaneEnabled: true,
+            navContentPaneEnabled: true
+        }
+    };
+
+    const reportContainer = document.getElementById('reportContainer');
+    console.log("reportContainer:", reportContainer);
+
+    if (reportContainer) {
+        const report = powerbi.embed(reportContainer, config);
+        console.log("Reporte embebido exitosamente.");
+
+        // Forzar la actualización del informe embebido
+        report.refresh().then(() => {
+            console.log("Informe actualizado.");
+        }).catch(error => {
+            console.error("Error al actualizar el informe:", error);
+        });
+    } else {
+        console.error("No se pudo encontrar el contenedor del reporte.");
+    }
+});
+
+
+
+
+document.getElementById('logoutButton').addEventListener('click', () => {
+    // Detener el proceso de Power BI
+    const reportContainer = document.getElementById('reportContainer');
+    if (reportContainer) {
+        powerbi.reset(reportContainer); // Limpia el contenedor del reporte embebido
+        console.log("Proceso de Power BI detenido.");
+    }
+
+    // Prevenir el uso del botón de retroceso para volver a esta página
+    history.pushState(null, null, location.href);
+    window.onpopstate = function () {
+        history.go(1);
+    };
+
+    // Redireccionar al usuario a la página de inicio de sesión
+    window.location.href = '/login.html';
+});
